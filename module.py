a00106fe5bdd5bdcd51b354a425726cbb7d968df
Sean <sdahl.sean@gmail.com>
2019-12-03 21:30:44 +0100

    documentation for module.py file

--- module.py
+++ module.py
@@ -124,7 +124,8 @@ class Ranger(tf.keras.optimizers.Optimizer):
                         learning rate at which the optimizer starts
                 """
                 super(RAdam, self).__init__(name='RAdam')
-                self._set_hyper('learning_rate', kwargs.get('lr', learning_rate))
+                # set the hyper parameters for the RAdam optimizer
+                self._set_hyper('learning_rate', kwargs.get('lr', learning_rate))  # learning rate set through learning_rate or lr argument
                 self._set_hyper('beta_1', beta_1)
                 self._set_hyper('beta_2', beta_2)
                 self._set_hyper('decay', self._initial_decay)
@@ -138,6 +139,9 @@ class Ranger(tf.keras.optimizers.Optimizer):
                 self._initial_total_steps = total_steps
 
             def _create_slots(self, var_list):
+                """
+                    function that needs to be implemented for every tf.keras optimizer
+                """
                 for var in var_list:
                     self.add_slot(var, 'm')
                 for var in var_list:
@@ -147,6 +151,9 @@ class Ranger(tf.keras.optimizers.Optimizer):
                         self.add_slot(var, 'vhat')
 
             def set_weights(self, weights):
+                """
+                    function that needs to be implemented for every tf.keras optimizer
+                """
                 params = self.weights
                 num_vars = int((len(params) - 1) / 2)
                 if len(weights) == 3 * num_vars + 1:
@@ -154,103 +161,156 @@ class Ranger(tf.keras.optimizers.Optimizer):
                 super(RAdam, self).set_weights(weights)
 
             def _resource_apply_dense(self, grad, var):
+                """
+                    function that updates the inner weigths by using RAdam optimization
+                    ...
+                    Parameters
+                    ----------
+                    grad : Tensor
+                        contains all the gradients for every variable
+                    var : Tensor
+                        contains all the variables to be trained for the model
+                """
                 var_dtype = var.dtype.base_dtype
-                lr_t = self._decayed_lr(var_dtype)
-                m = self.get_slot(var, 'm')
-                v = self.get_slot(var, 'v')
+                lr_t = self._decayed_lr(var_dtype)  # calculate a decayed learning rate (depending on iteration)
+                m = self.get_slot(var, 'm')  # get the momentums for the variables
+                v = self.get_slot(var, 'v')  # get the variances for the variables
+                # get hyper parameters of the optimizer
                 beta_1_t = self._get_hyper('beta_1', var_dtype)
                 beta_2_t = self._get_hyper('beta_2', var_dtype)
                 epsilon_t = tp.ops.convert_to_tensor(self.epsilon, var_dtype)
                 local_step = tp.math_ops.cast(self.iterations + 1, var_dtype)
+                # transform betas into version to the power of the number of previous optimization steps
                 beta_1_power = tp.math_ops.pow(beta_1_t, local_step)
                 beta_2_power = tp.math_ops.pow(beta_2_t, local_step)
+                # learning rate scheduling for the model
+                # only if scheduling iterations is set to be more than no iterations
                 if self._initial_total_steps > 0:
-                    total_steps = self._get_hyper('total_steps', var_dtype)
-                    warmup_steps = total_steps * self._get_hyper('warmup_proportion', var_dtype)
+                    total_steps = self._get_hyper('total_steps', var_dtype)  # total number of training steps for which to use learning rate scheduling
+                    warmup_steps = total_steps * self._get_hyper('warmup_proportion', var_dtype)  # get the number of steps for which to do warmup (increase of learning rate)
                     min_lr = self._get_hyper('min_lr', var_dtype)
-                    decay_steps = K.maximum(total_steps - warmup_steps, 1)
-                    decay_rate = (min_lr - lr_t) / decay_steps
+                    decay_steps = K.maximum(total_steps - warmup_steps, 1)  # calculate the number of steps over which to decrease learning rate
+                    decay_rate = (min_lr - lr_t) / decay_steps  # calculate the decrease of learning rate per step
+                    # set learning rate to value depending on wheter it is in warmup or learnig rate decreasing
                     lr_t = tf.where(
-                        local_step <= warmup_steps,
-                        lr_t * (local_step / warmup_steps),
-                        lr_t + decay_rate * K.minimum(local_step - warmup_steps, decay_steps),
+                        local_step <= warmup_steps,  # if in warmup then set lr to first option else to second option
+                        lr_t * (local_step / warmup_steps),  # learning rate is linearly increased from min_lr to lr_t over the course of the warmup iterations
+                        lr_t + decay_rate * K.minimum(local_step - warmup_steps, decay_steps),  # learning rate is linearly decreased for all the remaining scheduling iterations to min_lr
                     )
-                sma_inf = 2.0 / (1.0 - beta_2_t) - 1.0
-                sma_t = sma_inf - 2.0 * local_step * beta_2_power / (1.0 - beta_2_power)
-                m_t = tp.state_ops.assign(m, beta_1_t * m + (1.0 - beta_1_t) * grad, use_locking=self._use_locking)
-                m_corr_t = m_t / (1.0 - beta_1_power)
+                sma_inf = 2.0 / (1.0 - beta_2_t) - 1.0  # constant needed for optimization
+                sma_t = sma_inf - 2.0 * local_step * beta_2_power / (1.0 - beta_2_power)  # transform constant into variable depending on iteration
+                m_t = tp.state_ops.assign(m, beta_1_t * m + (1.0 - beta_1_t) * grad, use_locking=self._use_locking)  # calculate the new momentum for the current iteration
+                m_corr_t = m_t / (1.0 - beta_1_power)  # perform bias correction on the momentum (because it is to close to 0 for early iterations)
 
-                v_t = tp.state_ops.assign(v, beta_2_t * v + (1.0 - beta_2_t) * tp.math_ops.square(grad), use_locking=self._use_locking)
+                v_t = tp.state_ops.assign(v, beta_2_t * v + (1.0 - beta_2_t) * tp.math_ops.square(grad), use_locking=self._use_locking)  # calculate the new variances for the iteration
+                # for amsgrad version of Adam
                 if self.amsgrad:
+                    # calculate new values for variables vhat, bias corrected variances
                     vhat = self.get_slot(var, 'vhat')
                     vhat_t = tp.state_ops.assign(vhat, tp.math_ops.maximum(vhat, v_t), use_locking=self._use_locking)
                     v_corr_t = tp.math_ops.sqrt(vhat_t / (1.0 - beta_2_power))
                 else:
                     vhat_t = None
-                    v_corr_t = tp.math_ops.sqrt(v_t / (1.0 - beta_2_power))
-                r_t = tp.math_ops.sqrt((sma_t - 4.0) / (sma_inf - 4.0) * (sma_t - 2.0) / (sma_inf - 2.0) * sma_inf / sma_t)
-                var_t = tf.where(sma_t > 4.0, r_t * m_corr_t / (v_corr_t + epsilon_t), m_corr_t)
+                    v_corr_t = tp.math_ops.sqrt(v_t / (1.0 - beta_2_power))  # perform bias correction for the variances
+                r_t = tp.math_ops.sqrt((sma_t - 4.0) / (sma_inf - 4.0) * (sma_t - 2.0) / (sma_inf - 2.0) * sma_inf / sma_t)  # calculate new constant depending on the choice of hyper parameters and current iteration
+                var_t = tf.where(sma_t > 4.0, r_t * m_corr_t / (v_corr_t + epsilon_t), m_corr_t)  # calculate new value for the variables in the model using the previously calculated values
+                # decay weigths of the model if weight decay is non zero
                 if self._initial_weight_decay > 0.0:
                     var_t += self._get_hyper('weight_decay', var_dtype) * var
-                var_update = tp.state_ops.assign_sub(var,
-                                                  lr_t * var_t,
-                                                  use_locking=self._use_locking)
+                # perform actual updates
+                var_update = tp.state_ops.assign_sub(
+                    var,
+                    lr_t * var_t,
+                    use_locking=self._use_locking
+                )
                 updates = [var_update, m_t, v_t]
                 if self.amsgrad:
                     updates.append(vhat_t)
                 return tp.control_flow_ops.group(*updates)
 
             def _resource_apply_sparse(self, grad, var, indices):
+                """
+                    function that updates the inner weigths by using RAdam optimization
+                    ...
+                    Parameters
+                    ----------
+                    grad : Tensor
+                        contains all the gradients for every variable
+                    var : Tensor
+                        contains all the variables to be trained for the model
+                """
                 var_dtype = var.dtype.base_dtype
-                lr_t = self._decayed_lr(var_dtype)
+                lr_t = self._decayed_lr(var_dtype)  # calculate a decayed learning rate (depending on iteration)
+                m = self.get_slot(var, 'm')  # get the momentums for the variables
+                v = self.get_slot(var, 'v')  # get the variances for the variables
+                # get hyper parameters of the optimizer
                 beta_1_t = self._get_hyper('beta_1', var_dtype)
                 beta_2_t = self._get_hyper('beta_2', var_dtype)
                 epsilon_t = tp.ops.convert_to_tensor(self.epsilon, var_dtype)
                 local_step = tp.math_ops.cast(self.iterations + 1, var_dtype)
+                # transform betas into version to the power of the number of previous optimization steps
                 beta_1_power = tp.math_ops.pow(beta_1_t, local_step)
                 beta_2_power = tp.math_ops.pow(beta_2_t, local_step)
+                # learning rate scheduling for the model
+                # only if scheduling iterations is set to be more than no iterations
                 if self._initial_total_steps > 0:
-                    total_steps = self._get_hyper('total_steps', var_dtype)
-                    warmup_steps = total_steps * self._get_hyper('warmup_proportion', var_dtype)
+                    total_steps = self._get_hyper('total_steps',
+                                                  var_dtype)  # total number of training steps for which to use learning rate scheduling
+                    warmup_steps = total_steps * self._get_hyper('warmup_proportion',
+                                                                 var_dtype)  # get the number of steps for which to do warmup (increase of learning rate)
                     min_lr = self._get_hyper('min_lr', var_dtype)
-                    decay_steps = K.maximum(total_steps - warmup_steps, 1)
-                    decay_rate = (min_lr - lr_t) / decay_steps
+                    decay_steps = K.maximum(total_steps - warmup_steps,
+                                            1)  # calculate the number of steps over which to decrease learning rate
+                    decay_rate = (min_lr - lr_t) / decay_steps  # calculate the decrease of learning rate per step
+                    # set learning rate to value depending on wheter it is in warmup or learnig rate decreasing
                     lr_t = tf.where(
-                        local_step <= warmup_steps,
+                        local_step <= warmup_steps,  # if in warmup then set lr to first option else to second option
                         lr_t * (local_step / warmup_steps),
+                        # learning rate is linearly increased from min_lr to lr_t over the course of the warmup iterations
                         lr_t + decay_rate * K.minimum(local_step - warmup_steps, decay_steps),
+                        # learning rate is linearly decreased for all the remaining scheduling iterations to min_lr
                     )
-                sma_inf = 2.0 / (1.0 - beta_2_t) - 1.0
-                sma_t = sma_inf - 2.0 * local_step * beta_2_power / (1.0 - beta_2_power)
-                m = self.get_slot(var, 'm')
-                m_scaled_g_values = grad * (1 - beta_1_t)
-                m_t = tp.state_ops.assign(m, m * beta_1_t, use_locking=self._use_locking)
-                with tp.ops.control_dependencies([m_t]):
-                    m_t = self._resource_scatter_add(m, indices, m_scaled_g_values)
-                m_corr_t = m_t / (1.0 - beta_1_power)
-                v = self.get_slot(var, 'v')
-                v_scaled_g_values = (grad * grad) * (1 - beta_2_t)
-                v_t = tp.state_ops.assign(v, v * beta_2_t, use_locking=self._use_locking)
-                with tp.ops.control_dependencies([v_t]):
-                    v_t = self._resource_scatter_add(v, indices, v_scaled_g_values)
+                sma_inf = 2.0 / (1.0 - beta_2_t) - 1.0  # constant needed for optimization
+                sma_t = sma_inf - 2.0 * local_step * beta_2_power / (
+                            1.0 - beta_2_power)  # transform constant into variable depending on iteration
+                m_t = tp.state_ops.assign(m, beta_1_t * m + (1.0 - beta_1_t) * grad,
+                                          use_locking=self._use_locking)  # calculate the new momentum for the current iteration
+                m_corr_t = m_t / (
+                            1.0 - beta_1_power)  # perform bias correction on the momentum (because it is to close to 0 for early iterations)
+
+                v_t = tp.state_ops.assign(v, beta_2_t * v + (1.0 - beta_2_t) * tp.math_ops.square(grad),
+                                          use_locking=self._use_locking)  # calculate the new variances for the iteration
+                # for amsgrad version of Adam
                 if self.amsgrad:
+                    # calculate new values for variables vhat, bias corrected variances
                     vhat = self.get_slot(var, 'vhat')
                     vhat_t = tp.state_ops.assign(vhat, tp.math_ops.maximum(vhat, v_t), use_locking=self._use_locking)
                     v_corr_t = tp.math_ops.sqrt(vhat_t / (1.0 - beta_2_power))
                 else:
                     vhat_t = None
-                    v_corr_t = tp.math_ops.sqrt(v_t / (1.0 - beta_2_power))
-                r_t = tp.math_ops.sqrt((sma_t - 4.0) / (sma_inf - 4.0) * (sma_t - 2.0) / (sma_inf - 2.0) * sma_inf / sma_t)
-                var_t = tf.where(sma_t >= 5.0, r_t * m_corr_t / (v_corr_t + epsilon_t), m_corr_t)
+                    v_corr_t = tp.math_ops.sqrt(v_t / (1.0 - beta_2_power))  # perform bias correction for the variances
+                r_t = tp.math_ops.sqrt((sma_t - 4.0) / (sma_inf - 4.0) * (sma_t - 2.0) / (
+                            sma_inf - 2.0) * sma_inf / sma_t)  # calculate new constant depending on the choice of hyper parameters and current iteration
+                var_t = tf.where(sma_t > 4.0, r_t * m_corr_t / (v_corr_t + epsilon_t),
+                                 m_corr_t)  # calculate new value for the variables in the model using the previously calculated values
+                # decay weigths of the model if weight decay is non zero
                 if self._initial_weight_decay > 0.0:
                     var_t += self._get_hyper('weight_decay', var_dtype) * var
-                var_update = self._resource_scatter_add(var, indices, tf.gather(-lr_t * var_t, indices))
+                # perform actual updates
+                var_update = tp.state_ops.assign_sub(
+                    var,
+                    lr_t * var_t,
+                    use_locking=self._use_locking
+                )
                 updates = [var_update, m_t, v_t]
                 if self.amsgrad:
                     updates.append(vhat_t)
                 return tp.control_flow_ops.group(*updates)
 
             def get_config(self):
+                """
+                    function that returns the setting of all the hyper parameters for the RAdam optimizer
+                """
                 config = super(RAdam, self).get_config()
                 config.update({
                     'learning_rate': self._serialize_hyperparameter('learning_rate'),
